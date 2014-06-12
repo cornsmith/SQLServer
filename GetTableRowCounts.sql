05a169948f0ee7ee591ec7b3ba8dedf43247f328
cornsmith <corn.smith@outlook.com>
2014-06-12 14:06:59 +1000

    Remove old naming

--- GetTableRowCounts.sql
+++
@@ -1,8 +0,0 @@
-SELECT t.name
-	,s.partition_number
-	,s.row_count
-FROM sys.dm_db_partition_stats AS s
-	INNER JOIN sys.tables AS t ON t.[object_id] = s.[object_id]
-GROUP BY t.name
-	,s.partition_number
-	,s.row_count;
